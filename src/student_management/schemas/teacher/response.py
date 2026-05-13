5645dfd61154b67eef3017ff7347ca3047355568
dewly-binh <binh.phan@dewly.ai>
2026-05-13 13:24:37 +0700

    Feat/binh add teacher schema (#4)
    * feat(schemas): add teacher request and response schemas
    
    * refactor(schemas): rename TeacherChangePassword to TeacherChangePasswordRequest

---
+++ src/student_management/schemas/teacher/response.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+
+from student_management.models.teacher import Status
+
+
+class TeacherResponse(BaseModel):
+    id: str
+    full_name: str
+    email: str
+    teacher_code: str
+    department: str | None
+    phone: str | None
+    status: Status
+
+
+class TeacherSummaryResponse(BaseModel):
+    id: str
+    ful_name: str
+    teacher_code: str
+    email: str
+    status: Status
